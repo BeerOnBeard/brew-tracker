8756afe08cf0ca2766f6e9cb3d794c7d0bc2e7f4
BeerOnBeard <repoguy156+github@gmail.com>
2020-02-02 13:30:03 -0500

    Adding ability to get a list of brews. Moving common page css to a separate file.

---
+++ src/brewsStaticDataAccess.js
@@ -0,0 +1,8 @@
+module.exports = {
+  getBrews() {
+    return [
+      { id: 'e2991b61-e0aa-4c9c-8a8e-6bc5e8d2e61c', name: 'Pale Ale 1' },
+      { id: 'a9a2a6e2-5747-4ee5-90ed-dea718c4083e', name: 'Pale Ale 2' }
+    ];
+  }
+};
--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const dataAccess = require('./brewsStaticDataAccess');
+
 app.use(express.static('public'));
+
+app.get('/brews', (req, res) => {
+  res.send(dataAccess.getBrews());
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
